8814470acc5e3803cedec37e557b8c573d90739b
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-02-26 13:50:48 -0800

    NavBar

--- Find-Anything/src/App.jsx
+++ Find-Anything/src/App.jsx
@@ -4,12 +4,14 @@ import viteLogo from '/vite.svg'
 import CardLayout  from './components/CardLayout.jsx'
 import Searchbar from './components/Searchbar.jsx'
 import './components/styles/App.css'
+import NavBar from './components/NavBar.jsx'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
+    <NavBar/>
     <Searchbar placeholder="Search for anything..." />
       <CardLayout>
         <img src="/vite.svg" alt="Vite Logo" />
