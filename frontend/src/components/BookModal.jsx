4146882672d18721bb964e99bfc33d947f63244c
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-03-23 10:57:27 -0700

    started book modal

---
+++ frontend/src/components/BookModal.jsx
@@ -0,0 +1,21 @@
+import { useState } from 'react'
+import {Link} from 'react-router-dom';
+
+function BookModal()
+{
+
+    const[isOpen, setOpen] = useState(false);
+
+
+
+
+
+    return(
+    
+        <>
+                <h1>xd</h1>
+        </>
+    );
+}
+
+export default BookModal;
--- frontend/src/components/Card.jsx
+++ frontend/src/components/Card.jsx
@@ -1,16 +1,25 @@
 import { useState } from "react"
 import "./styles/card.css"
+import BookModal from "./BookModal";
 
 
 function Card(props){
 
+     const [showModal, setShowModel] = useState(false);
+
+
+
      return(<>
-     <div className="card" onClick={() => console.log("clicked on: " + props.name)}> 
+     <div className="card" onClick={() => setShowModel(true)}> 
           <img src={props.cover} className="card-img"></img>
           <h3 className="card-title">{props.name}</h3>
           <h5 className = 'author'>{props.author}</h5>
           <p className="card-body">{props.body}</p>
      </div>
+
+
+          {showModal && <BookModal/>}
+
      </>);
 }
 
