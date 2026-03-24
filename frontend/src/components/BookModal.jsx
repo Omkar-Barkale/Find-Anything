2e8b440eebd657fbefa2d545c5b1b86b17154822
chany yoon <chanyeeyoon@gmail.com>
2026-03-24 15:30:26 -0700

    modal layout and comment improved

--- frontend/src/components/BookModal.jsx
+++ frontend/src/components/BookModal.jsx
@@ -10,30 +10,26 @@ function BookModal(props)
         <>
             <div id = "modalOverlay" onClick={props.onClose}>
                 <div id="modalStyle" onClick={(e) => e.stopPropagation()}>  {/*does not notify the parent to close since we only want it to close outside or x*/}
-                    <button id = "close" onClick={props.onClose}>x</button>
-                    <div id = "bookContainer">
-                        <img src={props.cover} id="bookCover"></img>
+                    <div id = "bookSide">
+                        <button id = "close" onClick={props.onClose}>x</button>
+                        <div id = "bookContainer">
+                            <img src={props.cover} id="bookCover"></img>
+                        </div>
+
+                        <div id = "bookInfo">
+                            <h4 id = "bookName">{props.name}</h4>
+                            <h5 id = "bookAuthor"> {props.author}</h5>
+                        </div>
                     </div>
-
-                    <div id = "bookInfo">
-                        <h4 id = "bookName">{props.name}</h4>
-                        <h5 id = "bookAuthor"> {props.author}</h5>
+                    <div id = "commentSide">
+                        <div id = "commentSection"> 
+                            <h3>No comments yet</h3>
+                        </div>
+                        <form id = "commentForm">
+                            <input type = "text" id = "commentInput" placeholder = "Write a comment..." autocomplete="off" ></input>
+                            <button  id = "postBtn" type = "submit">Post</button>
+                        </form>
                     </div>
-
-
-
-
-
-                    <div id = "commentSection"> 
-                        <h3>No comments yet</h3>
-                    </div>
-                    <form id = "commentForm">
-                        <input type = "text" id = "commentInput" placeholder = "Write a comment..." autocomplete="off" ></input>
-                        <button  id = "postBtn" type = "submit">Post</button>
-                    </form>
-
-
-
                 </div>
             </div>
         </>
