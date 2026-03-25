5d2a2e71c438f803577fdd8ec02aee834f43016f
Rares-Casian <rarescasian.david@gmail.com>
2026-03-25 12:49:31 -0700

    added js input validation for front end in js

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -6,6 +6,9 @@ function Login(){
     const[email, setEmail] = useState("");
     const[password, setPassword] = useState("");
     const[message, setMessage] = useState("");
+    const[badEmail, setbadEmailMessage] = useState("");
+    const[badPassword, setbadPasswordMessage] = useState("");
+
     const navigate = useNavigate();
 
     function authenticator(){
@@ -42,28 +45,31 @@ function Login(){
         });
     }
 
-
-
-    function handleSubmit(e){
+    function CheckInput(e){
         e.preventDefault();
         
+        setbadEmailMessage("");
+        setbadPasswordMessage("");
+
         const regex_email = /^(.+)@([^\.].*)\.([a-z]{2,})$/;
-        const regex_password = /^[a-zA-z]\w{8,16}/;
+        const regex_password = /^[a-zA-z]\w{8,16}$/;
         let validInput = true;
-        if(!regex_email.text(email)){
+        if(!regex_email.test(email)){
             //badly formated email
+            console.log("Email is bad");
+            setbadEmailMessage("Enter a Valid Email Address");
             validInput = false;
         }
-        if(!regex_password.text(password)){
+        if(!regex_password.test(password)){
             //badly formated password
+            console.log("password is bad");
+            setbadPasswordMessage("Password must be between 8 to 16 characters");
             validInput = false;
         }
 
-
-
-
-        
-
+        if(validInput){
+            //authenticator();
+        }
     }
 
 
@@ -72,21 +78,21 @@ function Login(){
         <div id='container'>
             <div className='glass-container'>
             <h2>Login</h2>
-            <form onSubmit={handleSubmit}>
+            <form onSubmit={CheckInput}>
                 <input 
                     type='text' 
                     placeholder='Email' 
                     value={email}
                     onChange={(e) => setEmail(e.target.value)}
                 />
-
+                {badEmail ? <span>{badEmail}</span> : <></>}
                 <input 
                     type='password' 
                     placeholder='Password' 
                     value={password}
                     onChange={(e) => setPassword(e.target.value)}
                 />
-
+                {badPassword ? <span>{badPassword}</span> : <></>}
                 <div id='preferences'>
                     <div id='Remember'>
                         <input type='checkbox'/>Remeber me
