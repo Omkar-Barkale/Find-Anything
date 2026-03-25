af9156b83824785bb8defe39705336c7bbf3fba0
Rares-Casian <rarescasian.david@gmail.com>
2026-03-25 13:59:55 -0700

    changed App.jsx back to normal

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,8 +14,8 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path = "*" element = {<Login/>}/>
-        <Route path = "/login/" element ={<Home/>}/>
+        <Route path = "/login/" element = {<Login/>}/>
+        <Route path = "*" element ={<Home/>}/>
       </Routes>
   </>
   )}
