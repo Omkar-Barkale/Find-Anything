9d06683898bc793cfadf584566aa4cab20a39cf4
Chanyee Yoon <chanyeeyoon@gmail.com>
2026-03-26 16:42:39 -0700

    Added type validation, still need to ensure user uploads img

--- frontend/src/components/FileUploader.jsx
+++ frontend/src/components/FileUploader.jsx
@@ -6,18 +6,29 @@ import './styles/FileUploader.css'
 export default function FileUploader(props) //we use prop since need to send parent the data of the image selected
 {
     const [selectedImage, setSelectedImage] = useState("");
-
+    
     function handleFileChange(e){
+
         if(e.target.files){ //checks filelist object
             const newFile = e.target.files[0]; //need this as state updates are async, so cant use file
-            setSelectedImage(newFile ? URL.createObjectURL(newFile) : undefined);
-            props.setFile(newFile);
+            if((newFile.type == 'image/png' || newFile.type == 'image/jpeg')) //TODO SIZE VALIDATION
+            {
+                setSelectedImage(newFile ? URL.createObjectURL(newFile) : undefined);
+                props.setFile(newFile); //pass file to parent
+            } 
+            else
+            {
+                e.target.value = '';
+                console.log('Invalid file type');
+            }
         }
+
+ 
     }
     return(
         <>
             <img id = "imgPreview" src = {selectedImage} width = "100" height = "100"></img>
-            <input id = "uploadFile" type = "file" onChange = {handleFileChange}></input>
+            <input id = "uploadFile" type = "file" onChange = {handleFileChange} ></input>
         </>
     )
 }
